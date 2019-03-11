af4c774e9807868a26f98b4b656c8dc67e58e885
Prince Kumar <46297619+prince21298@users.noreply.github.com>
2019-03-11 10:50:45 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,61 @@
+# user=input("app aapna age dalo\n")
+# if user>=5 and user<=17:
+# 	print "aap school ja sakte ho"
+# elif user>=18 and user<=20:
+# 	print "aap vote de sakte ho"
+# elif user>=21 and user<=23:
+# 	print " app car drive kar sakte ho"
+# elif user==24:
+# 	print "aap sahhadi krr sakte ho"
+# elif user>=25:
+# 	print "aap legally drink kar sakte ho"
+# else:
+# 	print "app abhi kuch mhi krr sakte ho app abhi bhoot chote ho"
+
+
+# user=input("app aapna age dalo\n")
+# if 5<user:
+# 	print "aap school ja sakte ho"
+#  18<user:
+# 	print "aap vote de sakte ho"
+# elif 21
+# 	print " app car drive kar sakte ho"
+# elif user==24:
+# 	print "aap sahhadi krr sakte ho"
+# elif user>=25:
+# 	print "aap legally drink kar sakte ho"
+# else:
+# 	print "app abhi kuch mhi krr sakte ho app abhi bhoot chote ho"
+							              # qustion *****
+# i=1
+# while i <100:
+#    print "*"*i
+#    i=i+1
+
+   # sum of even and odd to 100
+# a=0
+# even=0
+# odd=0
+# while a<100:
+# 	if a%2==0:
+# 		even=even+a
+# 	else:
+# 		odd = odd+a
+# 	a=a+1
+# print "even"+ str(even)
+# print "odd"+ str(odd)
+			
+
+			#how can we check user year is leap year or not leap year
+
+# user = input("enter your year")
+# if user%4==0:
+# 	if user%100==0:
+# 		if user%400==0:
+# 			print "ye leap year hai"
+# 		else:
+# 			print "ye leap year nhi hai"
+# 	else:
+# 		print "ye leap year hai"
+# else:
+# 	print "ye leap year nhi hai"
