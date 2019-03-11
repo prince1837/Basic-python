800778aeca9e99e547b06b2f7098a991ae72ab55
Prince Kumar <46297619+prince21298@users.noreply.github.com>
2019-03-11 10:49:12 +0530

    Add files via upload

---
+++ fi.py
@@ -0,0 +1,444 @@
+# user=input("inter your number to find a that's number his two numbers of square those we get output is equal to 81...........\n")
+# f=[]
+# for i in range(1,user):
+#     b=0
+#     d=0
+#     c=list(str(i))
+#     n=None
+#     j=0
+#     while j<len(c):
+#         b=b+int(c[j])**2
+#         d=d+1
+#         if d==len(c):
+#             if b==n:
+#                 break
+#             elif b!=81:
+#                 c=str(b)
+#                 n=b
+#                 continue
+#             else:
+#                 if b==81:
+#                     f.append(i)
+#         j=j+1
+# print f
+
+
+# user=raw_input(" enter your string word....\n")
+# list1="abcdefghijklmnopqrstuvwxyz"
+# for i in list1:
+# 	if i in user:
+# 		b=list1.replace(i,'')
+# 		list1=b		
+# print b9+
+
+
+# names = [{'name': 'Pralhad', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 20},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Bhavnesh', 'age': 18},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Rishabh', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Anurag', 'age': 19},
+#  {'name': 'Bhavnesh', 'age': 20},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anurag', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 19},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Anoop', 'age': 25},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anurag', 'age': 23},
+#  {'name': 'Bhavnesh', 'age': 22},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Rishabh', 'age': 20},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Pralhad', 'age': 16},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Rishabh', 'age': 16},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Rishabh', 'age': 19},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Anoop', 'age': 22},
+#  {'name': 'Rishabh', 'age': 16},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Pralhad', 'age': 15},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Anoop', 'age': 15},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Rishabh', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Rishabh', 'age': 20},
+#  {'name': 'Bhavnesh', 'age': 18},
+#  {'name': 'Anoop', 'age': 22},
+#  {'name': 'Anurag', 'age': 15},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Anurag', 'age': 25},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anoop', 'age': 15},
+#  {'name': 'Pralhad', 'age': 19},
+#  {'name': 'Rishabh', 'age': 21},
+#  {'name': 'Anurag', 'age': 18},
+#  {'name': 'Anurag', 'age': 21},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Anurag', 'age': 16},
+#  {'name': 'Pralhad', 'age': 21},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Anurag', 'age': 16},
+#  {'name': 'Rishabh', 'age': 22},
+#  {'name': 'Bhavnesh', 'age': 16},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Pralhad', 'age': 22},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Pralhad', 'age': 16},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Rishabh', 'age': 19},
+#  {'name': 'Rishabh', 'age': 21},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Anurag', 'age': 17},
+#  {'name': 'Pralhad', 'age': 24},
+#  {'name': 'Anurag', 'age': 15},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 24},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 22},
+#  {'name': 'Anoop', 'age': 25}]
+# list_name = []
+# for i in names:
+# 	if i['name'] not in list_name:
+# 		list_name.append(i['name'])
+# for i in list_name:
+# 	count=0
+# 	for j in names:
+# 		if i==j['name']:
+# 			count+=1
+# 	print i,count
+
+
+# lists = [
+# 	[1,2,3,4,5,6,7,8],
+# 	[8,9,10,45,32,12],
+# 	[56,78,90,54,32],
+# 	[76,45,32],
+# 	[100, 567, 987]
+# ]
+# s=[]
+# for i in lists:
+# 	t=0
+# 	for j in i:
+# 		t=t+j
+# 	s.append(t)
+# a=s[0]
+# for i in s:
+# 	if i>a:
+# 		a=i
+# b=s.index(a)
+# k=lists[b]
+# print k
+
+
+
+# names = [{'name': 'Pralhad', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 20},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Bhavnesh', 'age': 18},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Rishabh', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Anurag', 'age': 19},
+#  {'name': 'Bhavnesh', 'age': 20},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anurag', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 19},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Anoop', 'age': 25},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anurag', 'age': 23},
+#  {'name': 'Bhavnesh', 'age': 22},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Rishabh', 'age': 20},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Pralhad', 'age': 16},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Rishabh', 'age': 16},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Rishabh', 'age': 19},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Anoop', 'age': 22},
+#  {'name': 'Rishabh', 'age': 16},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Pralhad', 'age': 15},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Anoop', 'age': 15},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Rishabh', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Rishabh', 'age': 20},
+#  {'name': 'Bhavnesh', 'age': 18},
+#  {'name': 'Anoop', 'age': 22},
+#  {'name': 'Anurag', 'age': 15},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Anurag', 'age': 25},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anoop', 'age': 15},
+#  {'name': 'Pralhad', 'age': 19},
+#  {'name': 'Rishabh', 'age': 21},
+#  {'name': 'Anurag', 'age': 18},
+#  {'name': 'Anurag', 'age': 21},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Anurag', 'age': 16},
+#  {'name': 'Pralhad', 'age': 21},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Anurag', 'age': 16},
+#  {'name': 'Rishabh', 'age': 22},
+#  {'name': 'Bhavnesh', 'age': 16},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Pralhad', 'age': 22},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Pralhad', 'age': 16},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Rishabh', 'age': 19},
+#  {'name': 'Rishabh', 'age': 21},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Anurag', 'age': 17},
+#  {'name': 'Pralhad', 'age': 24},
+#  {'name': 'Anurag', 'age': 15},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 24},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 22},
+#  {'name': 'Anoop', 'age': 25}]
+# s=[]
+# for i in names:
+# 	count = 0
+# 	for j in names:
+# 		if i['name']==j['name']:
+#  			count+=1
+# 	a= i['name'],count
+# 	for j in a:
+# 		if j not in s:
+# 			s.append(j)
+# print s
+
+
+
+
+
+
+# names = [{'name': 'Pralhad', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 20},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Bhavnesh', 'age': 18},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Rishabh', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Anurag', 'age': 19},
+#  {'name': 'Bhavnesh', 'age': 20},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anurag', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 19},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Anoop', 'age': 25},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anurag', 'age': 23},
+#  {'name': 'Bhavnesh', 'age': 22},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Rishabh', 'age': 20},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Pralhad', 'age': 16},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Rishabh', 'age': 16},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Rishabh', 'age': 19},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Anoop', 'age': 22},
+#  {'name': 'Rishabh', 'age': 16},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Pralhad', 'age': 15},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Anoop', 'age': 15},
+#  {'name': 'Anoop', 'age': 16},
+#  {'name': 'Anoop', 'age': 19},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Rishabh', 'age': 17},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Rishabh', 'age': 20},
+#  {'name': 'Bhavnesh', 'age': 18},
+#  {'name': 'Anoop', 'age': 22},
+#  {'name': 'Anurag', 'age': 15},
+#  {'name': 'Rishabh', 'age': 18},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Anurag', 'age': 25},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Anoop', 'age': 15},
+#  {'name': 'Pralhad', 'age': 19},
+#  {'name': 'Rishabh', 'age': 21},
+#  {'name': 'Anurag', 'age': 18},
+#  {'name': 'Anurag', 'age': 21},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Anurag', 'age': 16},
+#  {'name': 'Pralhad', 'age': 21},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Anurag', 'age': 16},
+#  {'name': 'Rishabh', 'age': 22},
+#  {'name': 'Bhavnesh', 'age': 16},
+#  {'name': 'Pralhad', 'age': 20},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Pralhad', 'age': 22},
+#  {'name': 'Bhavnesh', 'age': 21},
+#  {'name': 'Anurag', 'age': 20},
+#  {'name': 'Pralhad', 'age': 16},
+#  {'name': 'Rishabh', 'age': 24},
+#  {'name': 'Pralhad', 'age': 18},
+#  {'name': 'Rishabh', 'age': 23},
+#  {'name': 'Rishabh', 'age': 19},
+#  {'name': 'Rishabh', 'age': 21},
+#  {'name': 'Pralhad', 'age': 23},
+#  {'name': 'Rishabh', 'age': 15},
+#  {'name': 'Bhavnesh', 'age': 17},
+#  {'name': 'Anurag', 'age': 17},
+#  {'name': 'Pralhad', 'age': 24},
+#  {'name': 'Anurag', 'age': 15},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 24},
+#  {'name': 'Anoop', 'age': 23},
+#  {'name': 'Pralhad', 'age': 25},
+#  {'name': 'Bhavnesh', 'age': 22},
+#  {'name': 'Anoop', 'age': 25}]
+# s=[]
+# x=[]
+# for i in names:
+# 	age_count = 0
+# 	for j in names:
+# 		if i['name']==j['name']:
+# 			if i['age']==j['age']:
+# 				age_count+=1
+# 	a= i['age'],i['name'],age_count
+# 	x.append(a)
+# for j in x:
+# 	if j not in s:
+# 		s.append(j)
+# print s
+
+
+
+
+# user=[16,32,24,33,22,42,38]
+# empty=[]
+# q=len(user)
+# j=0
+# while j<q:
+#     j=j+1
+#     s=user[0]
+#     for i in user:
+#         if i<s:
+#             s=i
+#     empty.append(s)
+#     d=user.index(s)
+#     user.pop(d)
+# print (empty)
+
+
+
+
+# n = [10, 11, 12, 13, 14, 17, 18, 19]
+# f=[]
+# i=0
+# j=0
+# for i in range(len(n)):
+# 	j=i
+# 	for j in range(j,len(n)):
+# 		d=n[i]+n[j]
+# 		if d==30:
+# 			s=[n[i],n[j]]
+# 			f.append(s)
+# print f
+
+
+
+
+
+# array = [1, "youtube",3,"vlc","media","player", 2, "jagan", "swarnkar", 4, "ratnam", "kumar", "singh", "sagar", 3, "kal", "kishne", "dekha"]
+# for i in array:
+# 	print i
+# 	if i==(int(i)):
+# 		print i
