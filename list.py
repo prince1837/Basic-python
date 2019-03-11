199fbb348ab2342ce9d3c0b98ca5b8dd0197aad5
Prince Kumar <46297619+prince21298@users.noreply.github.com>
2019-03-11 10:51:03 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,143 @@
+# 				# KBC QUETION
+# list= ["1. aaj ram ne kya khya hai", "2.aaj ram kaha gya tha", "3. delhi kha hai", "4. bihar ka mukhya fasal kya hai ", "5. ramlaal ram ke ghar me kys hai"]
+# option_1=["a.roti","b. market","a.out of india","a.chaana grain","a.maalik"]
+# option_2=["b.daal makhaani","b.delhi","b.in india","b.rice","b.member"]
+# option_3=["c.chawal","c. garden","c. in uttarpardesh","c.wheat","c.servant"]
+# option_4=["d.mutton","d.mall","d.bihar","d.pulses","d.owner"]
+# a=["a.roti","a. market","a.out of india","a.chana grain","a.maalik"]
+# b=["b.daal makhaani","b.delhi","b.in india","b.rice","b.member"]
+# c=["c.chawal","c. garden","c. in uttarpardesh","c.wheat","c.servant"]
+# d=["d.mutton","d.mall","d.bihar","d.pulses","d.owner"]
+# answer_key=["d","b","b","b","c"]
+# i=0
+# while i<len(list):
+# 	print list[i]
+# 	print a[i]
+# 	print b[i]
+# 	print c[i]
+# 	print d[i]
+# 	user=raw_input("enter the answer\n")
+# 	if answer_key[i]==user:
+# 		print "your answer is correct"
+# 	else:
+# 		print "your answer is not correct"
+# 		print "you lost"
+# 	i=i+1
+
+					# list		
+					#QUSTION 1
+
+# list = ["1. which country is delhi located in ?", "what is the capital of india ?"]
+# option_1=["a. india", "a. chandigarh"]
+# option_2= ["b. USA", "b. bhopal"]
+# option_3= ["c. france", "c. delhi"]
+# option_4= ["d. czech", "d. jaipur"]
+# a=["a. india", "a. chandigarh"]
+# b=["b. USA", "b. bhopal"]
+# c=["c. france", "c. delhi"]
+# d=["d. czech", "d. jaipur"]
+# answer_key=["a", "c"]
+# i=0
+# while i<len(list):
+# 	print list[i]
+# 	print a[i]
+# 	print b[i]
+# 	print c[i]
+# 	print d[i]
+# 	user= raw_input("enter your answer\n")
+# 	if user==answer_key[i]:
+# 		print "your answer is correct"
+# 	else:
+# 		print "your  answer is wrong"
+# 		print "you lose"
+# 	i=i+1
+
+				#QUSTION 1
+# number= [50, 40, 23, 70, 56, 12, 5, 7]
+# i= 0
+# while i<len(number):
+# 	i=i+1
+# 	print max(number)
+# 	break
+
+		#how to get max no.. in list without using max ...	
+# n=[1, 12, 34, 45, 56, 78, 9, 134]
+# a=0
+# b=n[0]
+# while a<len(n):
+# 	if n[a]>b:
+# 		b=n[a]
+# 	a=a+1
+# print b		
+
+
+       # palindrome
+
+# name= ['n', 'i', 't', 'i', 'n']
+# a=name[::-1]
+# if a==name:
+# 	print "ye palindrome hai"
+# else:
+# 	print "ye palindrome nhi hai"
+
+			#palindrome in user input
+# user=raw_input("app appna no.. dallo\n")
+# if user==user[::-1]:
+# 	print "ye palindrome hai"
+# else:
+# 	print "ye palindrome nhi hai"
+
+				# last question
+
+# n=[50, 40, 23, 70, 56, 12, 5, 10, 7]
+# a=0
+# b=0
+# c=0
+# while a<len(n):
+# 	if n[a]<40 and n[a]>20:
+# 		b=b+1
+# 		print b
+# 	a=a+1
+
+
+# 				how can we append 3 or 4 list in one list
+
+
+
+# a=[2,3,4,5,6,6,4,6,4,6]
+# b=[3,2,54,3,5,3,5,35,3,5,35]
+# c=[2,3,4,6,7,5,7,5,7,5,46,5,6,46]
+# new_list=[]
+# for i in a+b+c:
+# 	new_list.append(i)
+# print new_list
+
+
+
+
+# def add_numbers(number_x, number_y):
+#     number_sum = number_x + number_y
+#     return number_sum
+
+# sum1 = add_numbers(20, 40)
+# print sum1
+# print sum1
+# sum2 = add_numbers(560, 23)
+# a = 1234
+# b = 12
+# sum3 = add_numbers(a, b)
+# print sum2
+# print sum3
+# number_a = add_numbers(20, 40) / add_numbers(5, 1)
+# print number_a
+
+# a = 3
+# b = 9
+# c = a * b
+# while c > 0 :
+# 	if c % a == 0 and c % b == 0 :
+# 		v = c
+# 	c = c - 1
+# print v		
+
+
