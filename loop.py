36a04edf6629fa593ade9b71b508a9135308519b
Prince Kumar <46297619+prince21298@users.noreply.github.com>
2019-03-11 10:45:02 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,54 @@
+# user1=input("pehla no.. dalo")
+# user2=input("dusra no.. dalo")
+# a=0
+# b=0
+# while a<user2:
+# 	b=b+user1
+# 	a=a+1
+# print b
+
+
+# QUESTION
+ 
+# a=0
+# b=1
+# c=0
+# while a<100:
+# 	c=a+b
+# 	print a
+# 	a=b
+#  	b=c
+
+
+   # QUESTION
+
+# a=0
+# b=0
+# c=1
+# while a<100:
+# 	print a
+# 	a=b
+# 	b=c
+# 	c=a+b
+
+# how can we use user in fibonaciseries
+# user = input("app apna no.. dalle\n")
+# a=0
+# b=0
+# c=1
+# while a<user:
+# 	print a
+# 	a=b
+# 	b=c
+# 	c=b+a
+
+					#LCM find
+# a = 56
+# b = 34
+# c = a * b
+# while c > 0 :
+# 	if c % a == 0 and c % b == 0 :
+# 		v = c
+# 	c = c - 1
+# print v		
+
