53872af6d4b5b2def2634a0a148b33088fa65186
Prince Kumar <46297619+prince21298@users.noreply.github.com>
2019-03-11 10:50:21 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,306 @@
+		# qustion 1
+		# part 1
+
+# def ask_question():
+# 	print "who is the founder of facebook? "
+# 	print "-mark zuckburg"
+# 	print "-bill gates"
+# 	print "steve  jobs"
+# 	print "-larry page"
+
+# i=0
+# while i<5:
+# 	ask_question()
+# 	i=i+1
+
+
+#			part2
+
+# def ask_question():
+# 	print "who is the founder of facebook? "
+# 	print "-mark zuckburg"
+# 	print "-bill gates"
+# 	print "steve  jobs"
+# 	print "-larry page"
+# i=0
+# while i<100:
+# 	ask_question()
+# 	i=i+1
+				
+		# 			question 2
+
+# def function_print_lines():
+# 	print "mera naam rishabh hai"
+# 	print "mai navgurukul ka co-founder hu."
+# function_print_lines()
+
+			#question 3
+			#part1
+
+# def add_no (number1, number2):
+# 	print "dono ka sum hai", 
+# 	print number1 + number2
+# add_no(23, 45)	
+
+
+				#part2
+
+# def add_no (list1,list2):
+# 	a=0
+# 	for i in range(len(list1)):
+# 		a=list1[i]+list2[i]
+# 		print a
+# 		print "------------"
+# add_no([50, 60, 10], [10, 20, 13])
+	
+
+# def say_hello_language(name, language):
+#     if language == "hindi":
+#         print "Namaste ", name
+#         print "Aap kaise ho?"
+#     elif language == "punjabi":
+#         print "Sat sri akaal ", name
+#         print "Tuhada ki haal hai?"
+#     else:
+#         print "Hello ", name
+#         print "How are you?"
+# say_hello_language("rishabh","punjabi")
+
+
+
+# def function_print_lines():
+# 	print "mera naam rishabh hai "
+# 	print " aur mai navgurukul ka co-founder hun."
+# function_print_lines()	
+
+			#	question 4
+
+
+# def check_numbers(a, b):
+
+# 		if a%2==0 and b%2==0:
+# 			print "dono even hai"
+# 		else:
+# 			print "dono even nhi hai"
+# check_numbers(24,12)
+
+
+		#	question
+
+# def check_numbers(list1, list2):
+# 	i=0
+# 	while i<len(list2):
+# 		if list1[i]%2==0 and list2[i]%2==0:
+# 			print "dono even numbers hai"
+# 			print "--------------------------"
+# 		else:
+# 			print "dono even number nhi hai"
+			
+# 		i=i+1
+# check_numbers([2, 6, 18, 10, 3, 75], [6, 19, 24, 12, 3, 87])
+
+
+
+
+
+
+# def calculator(a,b,c):
+# 	if user3=="+":
+# 		return a+b
+# 	elif user3=="-":
+# 		return a-b
+# 	elif user3=="*":
+# 		return a*b
+# 	else:
+# 		return a%b
+# user1=input("enter yoyur first number  ")
+# user2=input("enter yoyur second number  ")
+# user3=raw_input("what you want to do ??")
+# print calculator(user1,user2,user3)
+
+
+
+
+
+
+# def add(a,b,c):
+# 	if c == "+":
+# 		return a+b
+# 	elif c=="*":
+# 		return a*b
+# 	elif c=="-":
+# 		return a-b
+# 	else:
+# 		return a%b
+# user=int(raw_input("enter your first no.."))
+# user1=int(raw_input("enter your second no.."))
+# c=user 
+# print add(user,user1,c)
+
+
+#def prince(t):
+# 	a = multiple_list[0]
+# 	b=multiple_list[1]
+# 	c=[ ]
+# 	i=0
+# 	while i<len(b):
+# 		d=a[i]+b[i]
+# 		c.append(d)
+# 		i=i+1
+# 	print c
+# multiple_list =([5, 10, 50, 20], [2, 20, 3, 5])
+# prince(multiple_list)
+
+
+
+
+				#two list append to multiplyl with fun
+
+# def prince(t):
+# 	a = multiple_list[0]
+# 	b=multiple_list[1]
+# 	c=[ ]
+# 	i=0
+# 	while i<len(b):
+# 		d=a[i]+b[i]
+# 		c.append(d)
+# 		i=i+1
+# 	print c
+# multiple_list =([5, 10, 50, 20], [2, 20, 3, 5])
+# prince(multiple_list)
+
+
+
+
+# def calculator(a,b,c):
+# 	if c=="+":
+# 		return a+b
+# 	elif c =="-":
+# 		return a-b
+# 	elif c=="*":
+# 		return a*b
+# 	else:
+# 		return a%b
+# user1=input("enter yoyur first number  ")
+# user2=input("enter yoyur second number  ")
+# list = ["+", "-", "*", "%"]
+# for i in list:
+# 	print calculator(user1,user2,i)	
+
+
+
+# def add(a,b,c):
+# 	if c == "+":
+# 		return a+b
+# 	elif c=="*":
+# 		return a*b
+# 	elif c=="-":
+# 		return a-b
+# 	else:
+# 		return a%b
+# user=int(raw_input("enter your first no.."))
+# user1=int(raw_input("enter your second no.."))
+# list1=["+","*","-","%"]
+# for i in list1:
+# 	print add(user,user1,i)
+
+#    					part 3
+
+# def calculator(a,b,c):
+# 	if c=="+":
+# 		return a+b
+# 	elif c=="-":
+# 		return a-b
+# 	elif c=="*":
+# 		return a*b
+# 	elif c=="/":
+# 		return a/b
+# new=[]
+# list1=[[5,6,8,7,5],[5,8,3,7,9]]
+# i=0
+# while i<len(list1[0]):
+# 	a=list1[0]
+# 	b=list1[1]
+# 	c="*"
+# 	new.append(calculator(a[i],b[i],c))
+# 	i+=1
+# print new
+
+
+
+# def add(a,b,c):
+# 	if c == "+":
+# 		return a+b
+# 	elif c=="*":
+# 		return a*b
+# 	elif c=="-":
+# 		return a-b
+# 	else:
+# 		return a%b
+# new=[]
+# list1=[[5,6,8,7,5],[5,8,3,7,9]]
+# i=0
+# while i<len(list1[0]):
+# 	a=list1[0]
+# 	b=list1[1]
+# 	c="*"
+# 	new.append(add(a[i], b[i], c))
+# 	i=i+1
+# print new
+
+
+
+# def add(a,b,c):
+# 	if c == "+":
+# 		return a+b
+# 	elif c=="*":
+# 		return a*b
+# 	elif c=="-":
+# 		return a-b
+# 	else:
+# 		return a%b
+# 		
+
+
+
+
+
+
+# user=raw_input("enter your password\n")
+# if len(user)>6 and len(user)<16:
+# 	if "@" in user:
+# 		if "2" in user or "8" in user :
+# 			if "A" in user or "z" in user:
+# 				print "password strong hai"
+# 		else:
+# 			print "password strongnhi hai"
+# 	else:
+# 		print "password strong nhi hai"
+# else:
+# 	print "password strong nhi hai"
+
+
+# import getpass
+# user=getpass.getpass("enter your password\n")
+# if len(user)>6 and len(user)<16:
+# 	if "@" in user:
+# 		if "2" in user or "8" in user :
+# 			if "A" in user or "z" in user:
+# 				print "password strong hai"
+# 		else:
+# 			print "password strongnhi hai"
+# 	else:
+# 		print "password strong nhi hai"
+# else:
+# 	print "password strong nhi hai"
+
+
+# def max_num(list):
+
+# 	a=list[0]
+# 	for i in range(len(list)):
+# 		if list[i]>a:
+# 			a=list[i]
+# 	print a
+
+# max_num([12,3,56,67,78,89,340,7])
